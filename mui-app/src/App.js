9764e5cbb83f30eaba80fd7231fc995d60fa2509
Divine Higgins <dhiggins94@gmail.com>
2022-04-18 22:06:31 -0400

    now starting project

--- mui-app/src/App.js
+++ mui-app/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, styled, Typography } from "@mui/material";
+import "./App.css";
+import { Add, Settings } from "@mui/icons-material";
 
+const BlueButton = styled(Button)(({theme})=>({
+  backgroundColor: "skyblue",
+  color: "#888",
+  margin: 5,
+  "&:hover": {
+    backgroundColor: "lightblue",
+  },
+  "&:disabled": {
+    backgroundColor: "gray",
+    color: "white",
+  },
+}));
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Settings
+      </Button>
+      <Button
+        startIcon={<Add />}
+        variant="contained"
+        color="success"
+        size="small"
+      >
+        Add new post
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+      <Typography variant="h1" component="p">
+        it uses h1 style, but it is a p tag
+      </Typography>
+
+      <BlueButton> 1st button</BlueButton>
+      <BlueButton>2nd button</BlueButton>
     </div>
   );
 }
